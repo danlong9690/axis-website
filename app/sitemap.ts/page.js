b09aa3a92e8d25b1cd44c2bcc58058b1f820378d
Daniel Long <danlong9690@gmail.com>
2026-03-08 10:25:19 -0400

    create sitemap

---
+++ app/sitemap.ts/page.js
@@ -0,0 +1,36 @@
+import { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = "https://axisstrategiesgroup.com"
+
+  return [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/340b-consulting`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/pharmacy-revenue-optimization`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/infusion-program-development`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/pharmacy-leadership-and-operations`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/executive-advisory-support`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/compliance-and-audit-readiness`,
+      lastModified: new Date(),
+    }
+  ]
+}
